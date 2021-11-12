9c6c7020bfd31f86132490b8e37854eb71f90706
Anish M Prasad <anish.m.prasad@gmail.com>
2021-11-12 18:12:47 +0530

    mathjax offline demo

---
+++ mathjax-offline-demo/public/mathjax.config.js
@@ -0,0 +1,73 @@
+window.MathJax = {
+	skipStartupTypeset: true,
+	messageStyle: 'none',
+	showMathMenu: true,
+	//jax: ['input/TeX', 'input/MathML', 'output/CommonHTML'],
+	extensions: ['tex2jax.js', 'mml2jax.js', 'MathMenu.js', 'MathZoom.js'],
+	TeX: {
+		extensions: ['AMSmath.js', 'AMSsymbols.js', 'noErrors.js', 'noUndefined.js'],
+	},
+	tex2jax: {
+		preview: 'none',
+		inlineMath: [
+			['$', '$'],
+			['\\(', '\\)'],
+		],
+	},
+	jax: ['input/TeX', 'input/MathML', 'output/SVG'],
+	SVG: {
+		fontCache: 'global',
+		scale: 100,
+		undefinedFamily: "STIXGeneral, 'Arial Unicode MS', serif",
+		font: 'STIX-Web',
+		mtextFontInherit: true,
+		matchFontHeight: true,
+	},
+	root: getRootPath(),
+	AuthorInit: function () {
+		window.MathJax.Hub.Register.MessageHook('Math Processing Error', function (message) {
+			//  do something with the error.  message[2] is the Error object that records the problem.
+			console.log('Math Processing Error', message);
+		});
+	},
+};
+
+function getRootPath() {
+	if (window.location.host.includes('development.embibe.com')) {
+		return '';
+	}
+	return '/institute/atg';
+}
+
+// <script type="text/x-mathjax-config">
+// 			window.MathJax.Hub.Config({
+// 				skipStartupTypeset: true,
+// 				messageStyle: 'none',
+// 				showMathMenu: true,
+// 				//jax: ['input/TeX', 'input/MathML', 'output/CommonHTML'],
+// 				extensions: ['tex2jax.js', 'mml2jax.js', 'MathMenu.js', 'MathZoom.js'],
+// 				TeX: {
+// 					extensions: ['AMSmath.js', 'AMSsymbols.js', 'noErrors.js', 'noUndefined.js'],
+// 				},
+// 				tex2jax: {
+// 					preview: 'none',
+// 					inlineMath: [
+// 						['$', '$'],
+// 						['\\(', '\\)'],
+// 					],
+// 				},
+// 				jax: ['input/TeX', 'input/MathML', 'output/SVG'],
+// 				SVG: {
+// 					fontCache: 'global',
+// 					scale: 100,
+// 					undefinedFamily: "STIXGeneral, 'Arial Unicode MS', serif",
+// 					font: 'STIX-Web',
+// 					mtextFontInherit: true,
+// 					matchFontHeight: true,
+// 				},
+// 			});
+// 			window.MathJax.Hub.Register.MessageHook('Math Processing Error', function (message) {
+// 				//  do something with the error.  message[2] is the Error object that records the problem.
+// 				console.log('Math Processing Error', message);
+// 			});
+// 		</script>
--- mathjax-offline-demo/src/App.js
+++ mathjax-offline-demo/src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+
 import "./v2.7.x/MathJax.js?config=TeX-AMS-MML_HTMLorMML&dummy=.js";
 import { useEffect } from 'react';
-
+// let MathJax.Ajax['config']['root']
 function App() {
+
   useEffect(() => {
     window.MathJax.Hub.Queue(['Typeset', window.MathJax.Hub]);
   },[])
+
   return (
     <div className="App">
         <div>
-
+          
         </div>
     </div>
   );
